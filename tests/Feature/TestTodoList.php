a296d15a0c9bb41ee0774bc16f88d3ff92b29140
Anto Akoke Victor <veeqanto@gmail.com>
2022-07-14 01:58:14 +0100

    get single todo list

--- tests/Feature/TestTodoList.php
+++ tests/Feature/TestTodoList.php
@@ -11,15 +11,27 @@ class TestTodoList extends TestCase
 {
     use RefreshDatabase;
 
+    private $list;
+
+    public function setUp(): void 
+    {
+        parent::setUp();
+        $this->list = TodoList::factory()->create();
+    }
+
     public function test_fetch_todo_list()
     {
-        //prepare
-        TodoList::factory()->create();
-        
-        //perform
         $response = $this->getJson(route('todo-list.fetch'));
-        
-        //predict
+
         $this->assertEquals(1, count($response->json()));
+    }
+
+    public function test_get_single_todo_list()
+    {
+        $response = $this->getJson(route('todo-list.show', $this->list->id))
+                    ->assertOk()
+                    ->json();
+                    
+        $this->assertEquals($response['name'], $this->list->name);
     }
 }
